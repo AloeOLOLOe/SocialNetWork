130794c77eaf18309374ab1ae01c9e869ac40bf4
Your Name <stseregin@gmail.com>
2020-10-28 17:51:46 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () =>{
+    return <header className='header'>
+            <img src={'https://dota2.ru/img/heroes/jakiro/portrait.jpg'}/>
+    </header>
+}
+export default Header;
