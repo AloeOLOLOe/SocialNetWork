af735297d26e182452aa0185a92202b1bbdd2edc
Your Name <stseregin@gmail.com>
2021-02-08 17:52:21 +0300

    15 lesson

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import './Header.css';
+import s from'./Header.module.css';
 
 const Header = () =>{
-    return <header className='header'>
+    return <header className={s.header}>
             <img src={'https://dota2.ru/img/heroes/jakiro/portrait.jpg'}/>
     </header>
 }
