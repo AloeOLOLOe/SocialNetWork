4bb6f1f5f1c4aff2f3dc387c7f24ce05d85af01b
Your Name <stseregin@gmail.com>
2021-02-19 00:07:06 +0300

    19 lessons add BrowserRouter,Route we can switch page  dialogs\profile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,11 +4,11 @@ import s from './Navbar.module.css';
 const Navbar = () => {
     return <nav className={s.nav}>
 
-        <div className={`${s.item} ${s.active}`}>
-            <a>Profile</a>
-        </div>
         <div className={s.item}>
-            <a>Messages</a>
+            <a href='/profile'>Profile</a>
+        </div>
+        <div className={`${s.item} ${s.active}`}>
+            <a href='/dialogs'>Messages</a>
         </div>
         <div className={s.item}>
             <a>News</a>
