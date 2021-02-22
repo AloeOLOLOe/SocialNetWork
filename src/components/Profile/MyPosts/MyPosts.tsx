ee62c15c84b9a5a7d5028994d255aa0df4cb6526
Your Name <stseregin@gmail.com>
2021-02-23 01:23:31 +0300

    23 lessons add props in dialogs(dialogitem and message) separate profile (add ProfileInfo) improved style profile (changed ava+description and MyPosts.tsx)

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import  s from './MyPosts.module.css'
+import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = () => {
     return (
-        <div>
+        <div className={s.postsBlock}>
             My post
             <div>
-                <textarea> </textarea>
-                <button>Add post</button>
+                <div>
+                    <textarea> </textarea>
+                </div>
+                <div>
+                    <button>Add post</button>
+                </div>
+
 
             </div>
             <div className={s.posts}>
-                <Post message='Hi, how are you?' likesCount='0' />
-                <Post message="It's my first post" likesCount='23' />
+                <Post message='Hi, how are you?' likesCount='0'/>
+                <Post message="It's my first post" likesCount='23'/>
             </div>
         </div>
     )
